97d8fbdf702c42f0e824d38bdd30ba25a480c73a
darencorp <nazar.mandebura@gmail.com>
2017-11-25 18:30:51 +0100

    about page disable logs fix css

--- react_pyramid/static/src/js/src/components/pages/About.js
+++ react_pyramid/static/src/js/src/components/pages/About.js
@@ -7,8 +7,23 @@ export default class About extends React.Component {
 
     render() {
         return (
-            <div>
-                About
+            <div className="uk-flex uk-flex-column uk-flex-middle uk-margin-large-top">
+                <div className="uk-text-large">
+                    Created by Nazariy Mandebura
+                </div>
+                <div className="uk-text-muted">
+                    darencorp: <a className="uk-link">https://github.com/darencorp</a>
+                </div>
+                <div className="uk-position-bottom-center uk-margin-large-bottom">
+                    <div className="uk-flex uk-flex-column uk-flex-middle">
+                        <div>
+                            This project for lesson Team Project
+                        </div>
+                        <div className="uk-text-small uk-text-muted">
+                            Rzeszow, 2017
+                        </div>
+                    </div>
+                </div>
             </div>
         )
     }
